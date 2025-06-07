7557b573b35a7a94d354aa1be0e38f4c5c10dc2d
Daksh Dhanagar <dakshoffical770@gmail.com>
2025-06-07 20:59:17 +0530

    Practice code

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import ProductTab from "./ProductTab";
+
+function Title() {
+  return <h1>My Name is Daksh</h1>;
+}
+
+function App() {
+  
+  return (
+    <>
+    <Title/>
+    <ProductTab/>
+    </>
+  );
+    
+  
+}
+
+export default App
---
+++ basic-react-app/src/Product.jsx
@@ -0,0 +1,15 @@
+import "./Product.css";
+
+function Product () {
+    return(
+        
+        <div className="Product">
+            <h3>Title</h3>
+            <h5>Price</h5>
+        </div>
+        
+       
+    );
+}
+
+export default Product;
